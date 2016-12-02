7b3670fee5fbc3e2f83f2f28292a8b45aed7411b
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-12-02 12:03:13 +0100

    feat: run commandline commands in nfsclient

--- nfs-client/run.sh
+++ nfs-client/run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh -e
 
-if [ $# -ne 3 ]
+if [ ! $# -gt 2 ]
   then
-    echo "Usage: run.sh server server-mount-point uid"
+    echo "Usage: run.sh server server-mount-point uid [script]"
     exit 1
 fi
 
 adduser -D -u $3 nfsuser
 mount -v -t nfs -o nfsvers=4,nolock $1:$2 /mnt/nfs
 
-su - nfsuser -c "cd `pwd`; /bin/sh"
+if [ $# -gt 3 ]
+  then
+    su - nfsuser -c "cd `pwd`; $4"
+  else
+    su - nfsuser -c "cd `pwd`; /bin/sh"
+fi
